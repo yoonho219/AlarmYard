2e8aff3231dddc654ee18636c47851ce8dd31216
yoonho <yoonho.shin@wavle.io>
2023-08-07 17:52:20 +0900

    같이 사용하는 이미지 파일 생성

---
+++ src/assets/images/logos/arrows.jsx
@@ -0,0 +1,9 @@
+import prevpage from "../../images/prevpage.svg"
+import nextpage from "../../images/nextpage.svg"
+import tennextpage from "../../images/tennextpage.svg"
+
+export {
+    prevpage,
+    nextpage,
+    tennextpage,
+};
---
+++ src/assets/images/logos/functions.jsx
@@ -0,0 +1,11 @@
+import onlikelogo from "../likelogo1.svg";
+import offlikelogo from "../likelogo2.svg";
+import sharelogo from "../sharelogo.svg";
+import scraplogo from "../scraplogo.svg";
+
+export {
+    onlikelogo,
+    offlikelogo,
+    sharelogo,
+    scraplogo,
+};
