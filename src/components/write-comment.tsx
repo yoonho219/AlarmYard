4212eb6d5e26c9d921f5009c80340f723e54d1d7
shinyoonho <yoonho.shin@wavle.io>
2023-08-18 19:00:06 +0900

    댓글 관련 기능 추가

--- src/components/write-comment.tsx
+++ src/components/write-comment.tsx
@@ -1,16 +1,182 @@
 import styled from "styled-components";
-import React from "react";
+import React, { useState } from "react";
 
-export default function WriteComment() {
+interface ICommentProps {
+    commentCnt: number;
+    refetch: () => void;
+}
+
+export default function Commenting({ commentCnt, refetch }: ICommentProps) {
+    const [input, setInput] = useState<string>('');
+
+    const commentInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setInput(e.target.value)
+    }
+
+    const postComment = () => {
+        if (input === '') {
+            alert('댓글을 작성해주세요.')
+        }
+        else {
+            setInput('')
+            alert("댓글이 등록되었습니다.")
+            refetch();
+        }
+    }
+
+    const updateComment = () => {
+        alert('댓글을 수정하시겠습니까?')
+    }
+    const deleteComment = () => {
+        alert('댓글을 삭제하시겠습니까?')
+    }
+    const reportCommnet = () => {
+        alert('댓글을 신고하시겠습니까?')
+    }
+
     return (
-        <BoxLayout>
-            <CommentForm>
-                <textarea placeholder="댓글을 작성하세요." />
-                <span>등록</span>
-            </CommentForm>
-        </BoxLayout>
+        <>
+            <PostBottom>
+                <PostCommentLayout>
+                    <PostCommentCount>
+                        <span>댓글 <span style={{ color: "blue" }}>{commentCnt}</span></span>
+                    </PostCommentCount>
+                    {commentCnt === 0 ? (
+                        <PostComment>등록된 댓글이 없습니다.</PostComment>
+                    ) : (
+                        <PostCommentsForm>
+                            <PostComments>
+                                <div>
+                                    <div className="sort">
+                                        <span className="writer">작성자</span>
+                                        <span className="date">2023.01.01</span>
+                                        <CommentButton>
+                                            <div onClick={() => updateComment()} className="update">수정</div>
+                                            <div onClick={() => deleteComment()}>삭제</div>
+                                        </CommentButton>
+                                    </div>
+                                    <p>도움이 되는 연구정보였습니다.감사합니다.</p>
+                                </div>
+                            </PostComments>
+                            <PostComments>
+                                <div>
+                                    <div className="sort">
+                                        <span className="writer">김**</span>
+                                        <span className="date">2023.01.01</span>
+                                        <CommentButton>
+                                            <div onClick={() => reportCommnet()}>신고</div>
+                                        </CommentButton>
+                                    </div>
+                                    <p>좋은 자료입니다.</p>
+                                </div>
+                            </PostComments>
+                        </PostCommentsForm>
+                    )}
+                    <BoxLayout>
+                        <CommentForm>
+                            <textarea
+                                value={input}
+                                onChange={commentInput}
+                                placeholder="댓글을 작성하세요."
+                            />
+                            <span onClick={postComment}>등록</span>
+                        </CommentForm>
+                    </BoxLayout>
+                </PostCommentLayout>
+            </PostBottom>
+        </>
     )
 }
+
+const PostBottom = styled.div`
+  border-top: solid 1px #dcdcdc;
+  border-bottom: solid 1px black;
+`;
+const PostCommentLayout = styled.div`
+  margin: 35px 30px;
+`;
+const PostCommentCount = styled.div`
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 18px;
+`;
+const PostComment = styled.div`
+  margin: 80px 0;
+  display: flex;
+  justify-content: center;
+  color: #626873;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 28px;
+`;
+const PostCommentsForm = styled.div`
+  margin-top: 29px;
+  :last-child {
+    margin-bottom: 20px;
+  }
+`;
+const PostComments = styled.div`
+  height: 152px;
+  border-radius: 10px;
+  background: #f4f8ff;
+  margin-top: 10px;
+  > div {
+    padding: 34px;
+  }
+  div.sort {
+    height: 14px;
+    display: flex;
+    position: relative;
+  }
+  span:first-child {
+    height: 10px;
+    border-right: solid 1px #d8dde5;
+    margin-right: 12px;
+    padding-right: 12px;
+    font-weight: 500;
+  }
+  span {
+    font-family: Pretendard;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 14px;
+  }
+  .writer {
+    color: #333;
+  }
+  .date {
+    color: #6a768c;
+  }
+  p {
+    width: 100%;
+    height: 100%;
+    color: #626873;
+    margin-top: 15px;
+    font-family: Pretendard;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 28px;
+  }
+`;
+const CommentButton = styled.div`
+  cursor: pointer;
+  height: 14px;
+  position: absolute;
+  right: 0;
+  color: #666;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 14px;
+  display: flex;
+  .update {
+    height: 10px;
+    margin-right: 12px;
+    padding-right: 12px;
+    border-right: solid 1px #d8dde5;
+  }
+`;
 
 const BoxLayout = styled.div`
     margin: 0 auto;
