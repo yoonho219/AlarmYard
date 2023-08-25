6de1f4656b0e750ef64a7cdcf7d5564093d19ae0
shinyoonho <yoonho.shin@wavle.io>
2023-08-25 18:49:55 +0900

    recoil 패키지 제거

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <BrowserRouter>
-        <RecoilRoot>
-            <App />
-        </RecoilRoot>
+        <App />
     </BrowserRouter>
 );
