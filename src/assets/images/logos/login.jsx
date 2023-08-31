78ee87ba7c54e8d26d3ef880d7ea5796986764c2
shinyoonho <yoonho.shin@wavle.io>
2023-08-31 18:52:16 +0900

    로그인 페이지 생성 및 소셜로그인 사진 추가

---
+++ src/assets/images/logos/login.jsx
@@ -0,0 +1,6 @@
+import kakao from "../kakao-login.svg";
+import naver from "../naver-login.svg";
+import google from "../google-login.svg";
+import apple from "../apple-login.svg";
+
+export { kakao, naver, google, apple };
