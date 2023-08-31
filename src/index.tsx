835e580117f97251f6b27f5ab3f58ac363f3d7d6
shinyoonho <yoonho.shin@wavle.io>
2023-08-31 18:51:02 +0900

    apollo 라이브러리 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'http://211.110.139.183:5900/graphql',
+    cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ApolloProvider>
 );
