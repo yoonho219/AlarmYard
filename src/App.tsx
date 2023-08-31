11cf9b20cc72d4035de3a45b6484c429bf7fe708
shinyoonho <yoonho.shin@wavle.io>
2023-08-31 18:49:10 +0900

    로그인 라우트 추가 및 정렬 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import { styled } from 'styled-components';
-import './App.css';
-import React from 'react';
-import Header from './screen/header/header';
-import Footer from './screen/footer/footer';
-import Inquire from './screen/inquire';
-import Main from './screen/notification/main';
-import Individual from './screen/notification/individual';
-import { Route, Routes } from 'react-router-dom';
+import { styled } from "styled-components";
+import "./App.css";
+import React from "react";
+import Header from "./screen/header/header";
+import Footer from "./screen/footer/footer";
+import Login from "./screen/login";
+import Inquire from "./screen/inquire";
+import Main from "./screen/notification/main";
+import Individual from "./screen/notification/individual";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-    return (
-        <>
-            <Header />
-            <Layout />
-            <Routes>
-                <Route path='/' element={<Main />} />
-                <Route path="/post/:id" element={<Individual />} />
-                <Route path='QnA' element={<Inquire />} />
-            </Routes >
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Layout />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/:id" element={<Individual />} />
+        <Route path="QnA" element={<Inquire />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 const Layout = styled.div`
@@ -28,6 +30,6 @@ const Layout = styled.div`
   margin: 0 auto;
   display: flex;
   flex-direction: column;
-`
+`;
 
 export default App;
