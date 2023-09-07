c38b3c3f299258cc3935d72adb7030abf39ad6b2
shinyoonho <yoonho.shin@wavle.io>
2023-09-07 14:25:50 +0900

    카카오 로그인 Route 및 script생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { styled } from "styled-components";
 import "./App.css";
 import React from "react";
-import Header from "./screen/header/header";
-import Footer from "./screen/footer/footer";
-import Login from "./screen/login";
-import Inquire from "./screen/inquire";
-import Main from "./screen/notification/main";
-import Individual from "./screen/notification/individual";
+import Header from "./pages/header/header";
+import Footer from "./pages/footer/footer";
+import Login from "./pages/login";
+import Inquire from "./pages/inquire";
+import Main from "./pages/notification/main";
+import Individual from "./pages/notification/individual";
 import { Route, Routes } from "react-router-dom";
+import KakaoLogin from "./auth/kakaologin";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Layout />
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/oauth/kakao" element={<KakaoLogin />} />
         <Route path="/" element={<Main />} />
         <Route path="/:id" element={<Individual />} />
         <Route path="QnA" element={<Inquire />} />
