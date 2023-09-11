9f53621c51596999afd7f37091146222e6eef6d7
shinyoonho <yoonho.shin@wavle.io>
2023-09-11 10:01:58 +0900

    AuthProvider&카카오 js키 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,30 @@ import "./App.css";
 import React from "react";
 import Header from "./pages/header/header";
 import Footer from "./pages/footer/footer";
-import Login from "./pages/login";
+import LoginPage from "./pages/login-page";
 import Inquire from "./pages/inquire";
 import Main from "./pages/notification/main";
 import Individual from "./pages/notification/individual";
 import { Route, Routes } from "react-router-dom";
-import KakaoLogin from "./auth/kakaologin";
+import KakaoLogin from "./components/auth/kakaologin";
+import { AuthProvider } from "./components/auth/provider";
+import NaverLogin from "./components/auth/naverlogin";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Header />
       <Layout />
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/oauth/kakao" element={<KakaoLogin />} />
+        <Route path="/oauth/naver" element={<NaverLogin />} />
         <Route path="/" element={<Main />} />
         <Route path="/:id" element={<Individual />} />
         <Route path="QnA" element={<Inquire />} />
       </Routes>
       <Footer />
-    </>
+    </AuthProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const client = new ApolloClient({
-    uri: 'http://211.110.139.183:5900/graphql',
-    cache: new InMemoryCache(),
+  uri: "http://211.110.139.183:5900/graphql",
+  cache: new InMemoryCache(),
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const googleClient = process.env.REACT_APP_GOOGLE_CLIENT_KEY;
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
-    <ApolloProvider client={client}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </ApolloProvider>
+  <ApolloProvider client={client}>
+    <GoogleOAuthProvider clientId={`${googleClient}`}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+  </ApolloProvider>
 );
